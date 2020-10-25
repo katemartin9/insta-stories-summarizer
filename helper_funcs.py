8d97ef9d87d8e6812461753fadfe92734f16b1a7
Kate Martin <katmartin.uk@gmail.com>
2020-10-25 20:08:43 +0000

    resized images and added func to preview images

--- helper_funcs.py
+++ helper_funcs.py
@@ -1,2 +1,8 @@
+import matplotlib.pyplot as plt
 
+
+def plot_color_image(image):
+    plt.imshow(image, interpolation="nearest")
+    plt.axis("off")
+    plt.show()
 
--- run.py
+++ run.py
@@ -1,4 +1,7 @@
 from files_ops import get_stories, check_file_type, convert_video_to_np
+from tensorflow import keras
+import tensorflow as tf
+from helper_funcs import plot_color_image
 
 
 if __name__ == '__main__':
@@ -7,6 +10,11 @@ if __name__ == '__main__':
     for key, vals in insta_dict.items():
         for val in vals:
             if check_file_type(val):
-                print(f'{key}: {convert_video_to_np(val).shape}')
+                images = convert_video_to_np(val)
+                plot_color_image(images[0])
+                images_resized = tf.image.resize_with_pad(images, 224, 224).numpy().astype(int)
+                plot_color_image(images_resized[0])
+
+    model = keras.applications.resnet50.ResNet50(weights="imagenet")
 
 
